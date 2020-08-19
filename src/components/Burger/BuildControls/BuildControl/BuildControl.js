259b876b6562f96414617702ff84e94382360b9c
Robert Muñoz Marzo <robertmunozmarzo@Roberts-MacBook-Air.local>
2020-08-19 14:32:16 +0200

    Make buttons in BuildControl functional and disable less button

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -5,8 +5,18 @@ import classes from './BuildControl.module.css';
 const buildControl = (props) => (
   <div className={classes.BuildControl}>
     <div className={classes.Label}>{props.label} </div>
-    <button className={classes.Less}>More</button>
-    <button className={classes.More}>Less</button>
+    <button
+      className={classes.More}
+      onClick={props.removed}
+      disabled={props.disabled}
+    >
+      Less
+    </button>
+    <button
+    className={classes.Less}
+    onClick={props.added}>
+      More
+    </button>
   </div>
 );
 
